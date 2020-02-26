da396659e4e50b5c506f71b0bd8587bb99750741
Ascaliko <ascaliko7@gmail.com>
2020-02-26 18:02:26 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  let firstPageRef = $('a.menu').attr('href');      
+  callPage(firstPageRef);
+
+  $('a.menu').on('click', function(e){  
+      e.preventDefault();
+      var pageRef = $(this).attr('href');       
+      callPage(pageRef);
+  });
+
+});
+
+function callPage(pageRefInput) {
+  $.ajax({
+    url: pageRefInput,
+    type: "GET",
+    dataType: "text",
+    success: function( response ) {
+      console.log('the page was loaded', response);
+      $('.content').html(response);
+    },
+    error: function( error ) {
+      console.log('the page wasn\'t loaded', error);
+    },
+    complete: function( xhr, status ) {
+      console.log('the request is complete');
+    },
+  });
+}
