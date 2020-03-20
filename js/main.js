5697af2d41b8fdb660548bb8382350284fe020f0
Ascaliko <ascaliko7@gmail.com>
2020-03-20 17:27:20 +0700

    add portfolio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+console.log("%c Hey, Stop!", 
+  "font-weight: bold; font-size: 50px;color: red; "+
+  "text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
+console.log("%c don't go further", "font-weight: bold; font-size: 50px; color:red;")
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
            .register('./js/sw.js')
@@ -26,6 +30,7 @@ function callPage(pageRefInput) {
     },
     error: function( error ) {
       console.log('the page wasn\'t loaded', error);
+      $('.content').html('the page wasn\'t loaded');
     },
     complete: function( xhr, status ) {
       // console.log('the request is complete');
