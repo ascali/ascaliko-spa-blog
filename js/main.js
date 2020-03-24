7c09974b3a98cfc0dfae184a68400df5eb084d6a
Ascaliko <ascaliko7@gmail.com>
2020-03-24 17:04:47 +0700

    update portfolio

--- js/main.js
+++ js/main.js
@@ -8,6 +8,49 @@ if('serviceWorker' in navigator) {
            .then(function() { console.log("Service Worker 👷 Registered"); });
 }
 
+(function() {
+  if (!localStorage.getItem('cookieconsent')) {
+    document.body.innerHTML += '\
+    <div class="cookieconsent" style="position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
+      This site uses cookies. By continuing to use this website, you agree to their use. \
+      <a href="#" style="color:#CCCCCC;">I Understand</a>\
+    </div>\
+    ';
+    document.querySelector('.cookieconsent a').onclick = function(e) {
+      e.preventDefault();
+      document.querySelector('.cookieconsent').style.display = 'none';
+      localStorage.setItem('cookieconsent', true);
+    };
+  }
+})();
+
+/*(function() {
+  if (!localStorage.getItem('cookieconsent')) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(request.responseText);
+        var eu_country_codes = ['AL','AD','AM','AT','BY','BE','BA','BG','CH','CY','CZ','DE','DK','EE','ES','FO','FI','FR','GB','GE','GI','GR','HU','HR','IE','IS','IT','LT','LU','LV','MC','MK','MT','NO','NL','PO','PT','RO','RU','SE','SI','SK','SM','TR','UA','VA'];
+        if (eu_country_codes.indexOf(data.countryCode) != -1) {
+          document.body.innerHTML += '\
+          <div class="cookieconsent" style="position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
+            This site uses cookies. By continuing to use this website, you agree to their use. \
+            <a href="#" style="color:#CCCCCC;">I Understand</a>\
+          </div>\
+          ';
+          document.querySelector('.cookieconsent a').onclick = function(e) {
+            e.preventDefault();
+            document.querySelector('.cookieconsent').style.display = 'none';
+            localStorage.setItem('cookieconsent', true);
+          };
+        }
+      }
+    };
+    request.open('GET', 'http://ip-api.com/json', true);
+    request.send();
+  }
+})();*/
+
 $(document).ready(function(){
   let firstPageRef = $('a.menu').attr('href');      
   callPage(firstPageRef);
@@ -17,6 +60,13 @@ $(document).ready(function(){
       var pageRef = $(this).attr('href');       
       callPage(pageRef);
   });
+
+  /*setTimeout(function () {
+      $("#cookieConsent").fadeIn(200);
+   }, 4000);
+  $("#closeCookieConsent, .cookieConsentOK").click(function() {
+      $("#cookieConsent").fadeOut(200);
+  });*/
 });
 
 function callPage(pageRefInput) {
